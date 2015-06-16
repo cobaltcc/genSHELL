8ac8024274f62b999a7cd63a35712d652b2981d9
Ali Soltani <110king.pl@gmail.com>
2015-06-16 14:23:14 -0700

    Enables gravity

--- mfiles/createMODEL.m
+++ mfiles/createMODEL.m
@@ -1,7 +1,9 @@
 function createMODEL()
 % 
-% disp(sprintf('''MODEL'''));
-% disp(sprintf('GRAVDI 3'));
-% disp(sprintf('GRAVAC  -3.86220E+02'));
+disp(sprintf('''MODEL'''));
+disp(sprintf('GRAVDI 4'));
+disp(sprintf('GRAVAC  3.86220E+02'));
+disp(sprintf('RAYLEI  6.28318E-02   7.92000E-04'));
+
 
 end
